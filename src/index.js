9e5c878961750986a3d916475e1c5d78085972b7
Dima Ponomarev <kikkyow18@gmail.com>
2021-02-24 17:43:19 +0400

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  // if (str.length % 2 !== 0){
+  //   return false;
+  // }
+  // while (str !== ''){
+  //   let found = false;
+  //   for (brackets of bracketsConfig){
+  //     if (str.includes(brackets[0] + brackets[1])){
+  //       str.replace(brackets[0] + brackets[1], '');
+  //       found = true;
+  //     }
+  //   }
+  //   if (!found){
+  //     break;
+  //   }
+  // }
+  // if (str === ''){
+  //   return true;
+  // } else {
+  //   return false;
+  // }
+  while(str !== ''){
+    let found = false;
+    for (i of bracketsConfig) {
+        let brackets = i[0] + i[1];
+        if (str.includes(brackets)){
+            str = str.replace(brackets, '');
+            found = true;
+        }
+    }
+    if (!found) {
+      break;
+    };
+  }
+  if (str === '') {
+    return true;
+  } else {
+    return false;
+  }
+} 
